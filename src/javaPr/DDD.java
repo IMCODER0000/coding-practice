f7b4f60b28a02319c160feeba1e32b7f837cfa12
IMCODER0000 <gustn9025@naver.com>
2025-05-08 09:17:06 +0900

    2025-05-08

--- src/javaPr/DDD.java
+++ src/javaPr/DDD.java
@@ -1,4 +1,48 @@
 package javaPr;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class DDD {
+
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+
+
+        int[] result = new int[N];
+
+
+        for(int q = 0; q<M; q++){
+            String[] Ms = br.readLine().split(" ");
+            int i = Integer.parseInt(Ms[0]);
+            int j = Integer.parseInt(Ms[1]);
+            int k = Integer.parseInt(Ms[2]);
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
 }

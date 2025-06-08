c685b604baea70767b8d75849e84708631a1d6bb
DL-Choi <ggcura259@gmail.com>
2025-06-08 19:45:13 +0900

    2025-06-08

--- src/dfs/gold3/Main9466.java
+++ src/dfs/gold3/Main9466.java
@@ -7,38 +7,25 @@ import java.util.*;
 public class Main9466 {
 
 
-    static HashMap<Integer,Integer> graph;
+    static int[] graph;
     static boolean[] visited;
-
+    static boolean[] finished;
     static int teamCount;
 
-    static int startNode;
-
-    static int DFSCount;
-
-    static Set<Integer> tempArr;
 
     static void DFS(int node){
         visited[node] = true;
-        tempArr.add(node);
-        int i = graph.get(node);
-
-        if(i == node){
-            teamCount++;
-            return;
-        } else if(tempArr.contains(i)){
-            for (int q = 0; q<i; q++) {
-                tempArr.remove(q);
+        int next = graph[node];
+        if(!visited[next]){
+            DFS(next);
+        } else if(!finished[next]){
+            for (int i = next; i != node; i = graph[i]) {
+                teamCount++;
             }
-            teamCount += tempArr.size();
-            return;
+            teamCount++;
         }
 
-//        if(visited[graph.get(i)] || visited[i] ){
-
-
-        DFSCount++;  //1 2
-        DFS(i);
+        finished[node] = true;
 
     }
 
@@ -52,30 +39,26 @@ public class Main9466 {
         for (int q = 0; q < T; q++) {
             int n = Integer.parseInt(br.readLine());
             String[] student = br.readLine().split(" ");
-            graph = new HashMap<>();
-            startNode = 0;
-            DFSCount = 0;
+            graph = new int[n+1];
+            finished = new boolean[n+1];
             visited = new boolean[n+1];
             teamCount = 0;
+            
 
 
 
             for (int i = 0; i < n; i++) {
-                graph.put(i+1, Integer.parseInt(student[i]));
+                graph[i+1] = Integer.parseInt(student[i]);
             }
 
 
             for (int i = 0; i <n; i++) {
                 if (!visited[i+1]){
-                    tempArr = new HashSet<>();
-                    startNode = i+1;
-                    DFSCount = 0;
                     DFS(i+1);
                 }
             }
 
-            // 1 2 3 4 5
-            // 2 3 4 5 3
+
 
             System.out.println(n - teamCount);
 
